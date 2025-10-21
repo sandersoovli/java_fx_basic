3bb3fe0501be61ded6a676464e2fa72adfe9d69f
sandersoovli <sander.soovali@voco.ee>
2025-10-21 16:12:54 +0300

    GridPane lay out

--- main/java/com/application/JavaFxApplication.java
+++ main/java/com/application/JavaFxApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -13,12 +14,13 @@ public class JavaFxApplication extends Application {
 
     @Override
     public void start(Stage window) {
-        HBox layout = new HBox();
-        layout.setSpacing(10);
+        GridPane layout = new GridPane();
 
-        layout.getChildren().add(new Label("first"));
-        layout.getChildren().add(new Label("second"));
-        layout.getChildren().add(new Label("third"));
+        for (int x = 1; x <= 3; x++){
+            for (int y = 1; y <= 3; y++){
+                layout.add(new Button("" + x + ", " + y), x, y);
+            }
+        }
 
         Scene view = new Scene(layout);
 
