4b83700dda577374ddd1513eaa6037a747ca5bfe
sandersoovli <sander.soovali@voco.ee>
2025-10-21 13:50:56 +0300

    create a simple buttons using JavaFX

--- main/java/com/application/JavaFxApplication.java
+++ main/java/com/application/JavaFxApplication.java
@@ -1,13 +1,27 @@
 package com.application;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class JavaFxApplication extends Application {
 
     @Override
     public void start(Stage window) {
-        window.setTitle("My first application");
+        Button button1 = new Button("This is a first button");
+
+        Button button2 = new Button("This is a second button");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button1);
+        componentGroup.getChildren().add(button2);
+
+        Scene scene = new Scene(componentGroup, 300, 200);
+
+        window.setScene(scene);
+        window.setTitle("Two Buttons Example");
         window.show();
     }
 
